32666b14d30395ebf6200a845dc8e6990f996546
Dante Passera <dante@smashingboxes.com>
2015-04-29 17:36:07 -0400

    Circles expand on beat

--- app/src/main/java/com/smashingboxes/circleanimationtest/MainActivity.java
+++ app/src/main/java/com/smashingboxes/circleanimationtest/MainActivity.java
@@ -2,6 +2,8 @@ package com.smashingboxes.circleanimationtest;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.view.animation.AlphaAnimation;
@@ -10,8 +12,25 @@ import android.view.animation.AnimationSet;
 import android.view.animation.ScaleAnimation;
 import android.widget.RelativeLayout;
 
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
+
 
 public class MainActivity extends Activity {
+
+    private static final float MAX_PEAK = 10f;
+    // one minute in ms
+    private static final int MINUTE = 60000;
+    private static final int PEAKS_PER_BEAT = 5;
+
+    private int BPM = 60; // 60 | 100 | 200
+    private ArrayList<Integer> mCircleIds = new ArrayList<>();
+    private int mBeatCount = 0;
+    // dummy peak amplitudes
+    private float[] PEAKS = {0.125f*MAX_PEAK, -0.125f*MAX_PEAK, 1*MAX_PEAK, -0.25f*MAX_PEAK, 0.1875f*MAX_PEAK};
+    private Timer mTimer;
+    private TimerTask mTimerTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +41,35 @@ public class MainActivity extends Activity {
     @Override
     protected void onResume() {
         super.onResume();
+        initCircles();
+        startLoop();
         addTapListener();
+    }
+
+    private void initCircles() {
+        mCircleIds.add(R.id.circle_p);
+        mCircleIds.add(R.id.circle_q);
+        mCircleIds.add(R.id.circle_r);
+        mCircleIds.add(R.id.circle_s);
+        mCircleIds.add(R.id.circle_t);
+
+        for(int i=0; i< mCircleIds.size(); i++) {
+            findViewById(mCircleIds.get(i)).setVisibility(View.INVISIBLE);
+        }
+    }
+
+    private void startLoop() {
+        final Handler handler = new BeatHandler();
+
+        mTimer = new Timer();
+        mTimerTask = new TimerTask() {
+            @Override
+            public void run() {
+                handler.sendEmptyMessage(BeatHandler.BEAT);
+            }
+        };
+
+        mTimer.scheduleAtFixedRate(mTimerTask, 0, MINUTE/BPM/PEAKS_PER_BEAT);
     }
 
     private void addTapListener() {
@@ -31,7 +78,8 @@ public class MainActivity extends Activity {
         view.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                scaleCircle(findViewById(R.id.circle_p), 0f, 1f, 1000);
+//                scaleCircle(findViewById(R.id.circle_p), 0f, 1f, 1000);
+                //TODO: step through BPMs
             }
         });
     }
@@ -51,12 +99,35 @@ public class MainActivity extends Activity {
 
         AnimationSet set = new AnimationSet(true);
         set.addAnimation(scale);
-        set.addAnimation(alpha);
+//        set.addAnimation(alpha);
         set.setFillBefore(true);
         set.setFillAfter(true);
         set.setFillEnabled(true);
         set.setInterpolator(new AccelerateDecelerateInterpolator());
 
+        circle.setVisibility(View.VISIBLE);
         circle.startAnimation(set);
     }
+
+    public class BeatHandler extends Handler
+    {
+        public static final int BEAT = 1;
+        private static final long dur = 500;
+
+        @Override
+        public void handleMessage(Message msg) {
+            switch(msg.what) {
+                case BEAT:
+                    float endScale = PEAKS[mBeatCount]/MAX_PEAK;
+                    scaleCircle(findViewById(mCircleIds.get(mBeatCount)),
+                            0, endScale, dur);
+                    mBeatCount ++;
+                    if(mBeatCount == PEAKS_PER_BEAT) {
+                        mBeatCount = 0;
+                    }
+                    break;
+            }
+        }
+    }
 }
+
